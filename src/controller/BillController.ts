e5c3b54c659dfda5ee86fa8abc8ff3e01544c926
GPrince00 <gustavoprince0@gmail.com>
2024-08-29 17:38:53 -0300

    feat(gemini): setup the google gemini

--- src/controller/BillController.ts
+++ src/controller/BillController.ts
@@ -1,8 +1,20 @@
-import { Request, Response } from "express"
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
+require('dotenv').config();
 export class BillController {
+  async save(image: string) {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    async save(request: Request, response: Response) {
-        return response.status(200).end();
-    }
+    const result = await model.generateContent([
+      "what is the measurement? I need just the numbers as an answer",
+      {
+        inlineData: {
+          data: image,
+          mimeType: "image/png",
+        },
+      },
+    ]);
+    console.log(result.response.text());
+    return;
+  }
 }
